475ef59886c8a19f4b7f6f94391f8754e9e21692
Cisco248 <lahirud608@gmail.com>
2024-02-29 06:43:09 +0530

    example

--- Lesson 14  Example for the Barrier/L14-example.c
+++ Lesson 14  Example for the Barrier/L14-example.c
@@ -7,8 +7,8 @@
 
 #define THREAD_NUM 8
 
-int diceValues[8];
-int status[8] = { 0 };
+int diceValues[THREAD_NUM];
+int status[THREAD_NUM] = { 0 };
 
 pthread_barrier_t barrierRolledDice;
 pthread_barrier_t barrierCalculated;
@@ -19,11 +19,12 @@ void* rollDice(void* args) {
         diceValues[index] = rand() % 6 + 1;
         pthread_barrier_wait(&barrierRolledDice);
         pthread_barrier_wait(&barrierCalculated);
+
         if (status[index] == 1) {
             printf("(%d Rolled %d) I Won\n", index, diceValues[index]);
         } 
         else {
-            printf("(%d Rolled %d) I Lost\n", index, diceValues[diceValues[index]]);
+            printf("(%d Rolled %d) I Lost\n", index, diceValues[index]);
         }   
     }
     free(args);
@@ -33,10 +34,11 @@ int main (int argc, char* argv[]) {
     pthread_t th[THREAD_NUM];
     pthread_barrier_init(&barrierRolledDice, NULL, THREAD_NUM + 1);
     pthread_barrier_init(&barrierCalculated, NULL, THREAD_NUM + 1);
+
     for (int i = 0; i < THREAD_NUM; i++) {
-            int* a =malloc(sizeof(int));
-            a = i;
-            if (pthread_create(&th[i], NULL, &rollDice, NULL) != 0) {
+            int* a = malloc(sizeof(int));
+            *a = i;
+            if (pthread_create(&th[i], NULL, &rollDice, a) != 0) {
                 perror("Failed to Create Thread !!!");
             }
     }
@@ -46,17 +48,17 @@ int main (int argc, char* argv[]) {
 
         int max = 0;
         for (int i = 0; i < THREAD_NUM; i++) {
-            if (diceValues[1] > max) {
-                max = diceValues[1];
+            if (diceValues[i] > max) {
+                max = diceValues[i];
             }
         }
 
         for (int i = 0; i < THREAD_NUM; i++) {
-            if (diceValues[1] == max) {
+            if (diceValues[i] == max) {
                 status[i] = 1;
             }
             else {
-                status[1] = 0;
+                status[i] = 0;
             }
         }
         printf("===== NEW ROUND STARTED =====\n");

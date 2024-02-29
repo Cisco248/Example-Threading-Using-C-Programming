1befaa2bfaca284adc43539749be8741e7a0ed70
Cisco248 <lahirud608@gmail.com>
2024-03-01 04:15:43 +0530

    last

--- Lesson - 20 Between Semaphores and Mutex/L20-thread2.c
+++ Lesson - 20 Between Semaphores and Mutex/L20-thread2.c
@@ -7,18 +7,16 @@ This Example Using the Semaphores
 #include <pthread.h>
 #include <time.h>
 #include <string.h>
-#include <semaphore.h>
 
-#define THREAD_NUM 4
+#define THREAD_NUM 2
 
-sem_t semFuel;
 pthread_mutex_t mutexFuel;
 
 int fuel = 50;
 
 void* routine1(void* args) {
     while (1) {
-        sem_wait(&semFuel);
+        pthread_mutex_lock(&mutexFuel);
         fuel += 50;
         printf("Current Volume : %d\n", fuel);
     }
@@ -26,14 +24,13 @@ void* routine1(void* args) {
 
 void* routine2(void* args) {
     while (1) {
-        sem_post(&semFuel);
+        pthread_mutex_unlock(&mutexFuel);
         usleep(100000);
     }
 }
 
 int main (int argc, char *argv[]) {
     pthread_t th[THREAD_NUM];
-    sem_init(&semFuel, 0, 1);
     pthread_mutex_init(&mutexFuel, NULL);
     int i;
 
@@ -54,7 +51,6 @@ int main (int argc, char *argv[]) {
             perror("Failed to Join Thread !!!");  
         }
     }
-    sem_destroy(&semFuel);
     pthread_mutex_destroy(&mutexFuel);
     return 0;
 }

1afdc5bbbcaa3b744a2537dcdf8a549455060845
Cisco248 <lahirud608@gmail.com>
2024-03-01 01:40:30 +0530

    semaphores

---
+++ Lesson - 19 Semaphores/L19-thread.c
@@ -0,0 +1,44 @@
+/*
+
+This Example Show the Semaphores
+--------------------------------
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+#include <string.h>
+#include <semaphore.h>
+
+#define THREAD_NUM 4
+
+sem_t semaphore;
+
+void* routine(void* args) {
+    printf("Hello! From Thread %d\n", *(int*)args);
+    free(args);
+}
+
+int main (int argc, char *argv[]) {
+    pthread_t th[THREAD_NUM];
+    sem_init(&semaphore, 0, 1);
+    int i;
+    for (i = 0; i < THREAD_NUM; i++) {
+        int* a = malloc(sizeof(int));
+        a* = i;
+        if (pthread_create(&th[i], NULL, &routine, a) != 0) {
+            perror("Failed to Create Thread !!!");
+        } 
+    }
+
+    for (i = 0; i < THREAD_NUM; i++) {
+        if (pthread_join(th[i], NULL) != 0) {
+            perror("Failed to Join Thread !!!");  
+        }
+    }
+    sem_destroy(&semaphore);
+    return 0;
+}
